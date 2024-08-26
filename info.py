07d9959d28dcbc5c7605de6d1572c1812f832e8d
hello123-jpg <jaskeet8bpyt@gmail.com>
2024-08-26 13:55:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,10 +18,10 @@ def is_enabled(value, default):
         return default
 
 # Bot information *
-SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+SESSION = environ.get('SESSION', 'PSO_AUTOBOT')
+API_ID = int(environ['7684469'])
+API_HASH = environ['7289a31b2b539f5c92f39fa1a3c3ad6c']
+BOT_TOKEN = environ['7518791498:AAFq982ctHMk71aFjRHyQBkQ-tZ-SOGS_dw']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
